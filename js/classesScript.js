660401153302440903712db546563d5b495772de
MichaelZSong <65313998+MichaelZSong@users.noreply.github.com>
2022-10-19 14:30:04 -0700

    Replaced course ID with age range

--- js/classesScript.js
+++ js/classesScript.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     $(add_button).click(function (e) {
         e.preventDefault();
         x++;
-        $(wrapper).append('<div id=\"class' + x + '\" class=\"container1\"><div class=\"center\"><span class=\"section-labels\">CLASS ' + x + '<\/span><\/div><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Course Name&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"className[]\" placeholder=\"Oil and Chalk Pastels\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Course ID&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classID[]\" placeholder=\"136426\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Day of the Week&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classDay[]\" placeholder=\"Tuesdays\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Start &#38; End Date&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classDate[]\" placeholder=\"Jun 14 - Jul 5, 2022\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Start &#38; End Time&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classTime[]\" placeholder=\"6:30 pm - 8:30 pm\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Link to Course&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classLink[]\" placeholder=\"https:\/\/cityofsurrey.perfectmind.com\/...\"><\/td><\/tr><\/tbody><\/table><hr><\/div>'); // add suspect field
+        $(wrapper).append('<div id=\"class' + x + '\" class=\"container1\"><div class=\"center\"><span class=\"section-labels\">CLASS ' + x + '<\/span><\/div><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Course Name&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"className[]\" placeholder=\"Oil and Chalk Pastels\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Age Range&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"ageRange[]\" placeholder=\"2 - 4\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Day of the Week&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classDay[]\" placeholder=\"Tuesdays\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Start &#38; End Date&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classDate[]\" placeholder=\"Jun 14 - Jul 5, 2022\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Start &#38; End Time&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classTime[]\" placeholder=\"6:30 pm - 8:30 pm\"><\/td><\/tr><\/tbody><\/table><table class=\"table-row\"><tbody><tr><td class=\"label-area\"><span class=\"labels\">Link to Course&#58;<\/span><\/td><td class=\"label-boxes\"><input type=\"text\" class=\"table-input table-input-form\" name=\"classLink[]\" placeholder=\"https:\/\/cityofsurrey.perfectmind.com\/...\"><\/td><\/tr><\/tbody><\/table><hr><\/div>'); // add suspect field
     });
 
     var remove_button = $(".remove_form_field1");
@@ -25,7 +25,7 @@ function generate() {
 
     // multiple length fields
     var className = [];
-    var classID = [];
+    var ageRange = [];
     var classDay = [];
     var classDate = [];
     var classTime = [];
@@ -35,8 +35,8 @@ function generate() {
     for (i = 0; i < output.length; i++) {
         if (output[i].name === "className[]") {
             className.push(output[i].value);
-        } else if (output[i].name === "classID[]") {
-            classID.push(output[i].value);
+        } else if (output[i].name === "ageRange[]") {
+            ageRange.push(output[i].value);
         } else if (output[i].name === "classDay[]") {
             classDay.push(output[i].value);
         } else if (output[i].name === "classDate[]") {
@@ -51,7 +51,7 @@ function generate() {
     // format the arrays into their final submit format
     var classes = [];
     for (i = 0; i < className.length; i++) {
-        classes[i] = "<h4><strong><a href=\"" + classLink[i] + "\">" + className[i] + " (" + classID[i] + ")<\/a><\/strong><\/h4>\r\n<em>" + classDay[i] + ", " + classDate[i] + ", " + classTime[i] + "<\/em>\r\n";
+        classes[i] = "<h4><strong><a href=\"" + classLink[i] + "\">" + className[i] + " (Age " + ageRange[i] + ")<\/a><\/strong><\/h4>\r\n<em>" + classDay[i] + ", " + classDate[i] + ", " + classTime[i] + "<\/em>\r\n";
     }
 
     var classCat = output.classCat.value;
